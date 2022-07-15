df533f4cd0c17e15cb548c8535f35df30fa78cd6
jonte <jonatan@jontes.page>
2022-07-15 11:25:21 +0200

    Fixing formatting as per request in PR #1 by Svemat. Also removing temporary files.

--- linux.sh
+++ linux.sh
@@ -1,5 +1,10 @@
 #!usr/bin/env bash
 
+# I am aware that this is objectively not a oneliner, but the install command is.
+# I am also aware that this is not the best way to install the software, but it works.
+# Thank you to all the people who have provided me with feedback for this.
+# To make a pull request, please generate a sha512 hash of the script files changed and add them to scriptname.sha512. Thank you.
+
 # Colors
 Purple='\033[0;35m'
 NC='\033[0m' # No Color
@@ -12,7 +17,7 @@ else
     exit 1
 fi
 
-MY_PATH=$(pwd)    # absolutized and normalized
+MY_PATH=$(pwd)
 if [[ -z "$MY_PATH" ]] ; then
     exit 1  # fail
 fi
@@ -47,7 +52,7 @@ arch=$(uname -i)
 echo "Checking latest version of Ventoy..."
 ventoy_version=$(curl https://api.github.com/repos/Ventoy/ventoy/releases/latest -s | jq .tag_name -r)
 echo "Downloading tarball..."
-curl -fsSL https://github.com/ventoy/Ventoy/releases/download/$ventoy_version/ventoy-1.0.78-linux.tar.gz > /tmp/ventoy.tar.gz
+curl -fsSL https://github.com/ventoy/Ventoy/releases/download/$ventoy_version/ventoy-1.0.78-linux.tar.gz >/tmp/ventoy.tar.gz
 echo "Comparing Checksums..."
 local_checksum=$(echo $(sha256sum /tmp/ventoy.tar.gz) | sed 's/ .*//')
 global_checksum=$(echo $(curl -fsSL https://github.com/ventoy/Ventoy/releases/download/$ventoy_version/sha256.txt) | grep --color=never ventoy-${ventoy_version:1}-linux.tar.gz | sed 's/ .*//')
@@ -60,9 +65,11 @@ else
 fi
 
 echo "Extracting tarball..."
-tar -xzvf /tmp/ventoy.tar.gz -C /tmp >> /tmp/ventoytool_untar.log
+tar -xzvf /tmp/ventoy.tar.gz -C /tmp >>/tmp/ventoytool_untar.log
 echo "Making Ventoy directory..."
-mkdir /home/$USER/Ventoy
+mkdir /home/$USER/Ventoyrm -rf /tmp/ventoy.tar.gz
+rm -rf /tmp/ventoy-${ventoy_version:1}
+rm -f /tmp/ventoytool_untar.log
 echo "Moving files..."
 mv /tmp/ventoy-${ventoy_version:1}/ventoy /home/$USER/Ventoy/ventoy
 mv /tmp/ventoy-${ventoy_version:1}/boot /home/$USER/Ventoy/boot
@@ -82,6 +89,10 @@ else
     exit 1
 fi
 
+echo "Removing temporary files"
+rm -rf /tmp/ventoy.tar.gz
+rm -rf /tmp/ventoy-${ventoy_version:1}
+rm -f /tmp/ventoytool_untar.log
 echo "Done! - Launching VentoyGUI. To run VentoyGUI afterwards, run the following command:"
 echo "/home/$USER/VentoyGUI"
 ~/Ventoy/VentoyGUI &
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 VentoyTool is a tool for quickly running the latest version of Ventoy with almost zero extra dependencies.  
 
 Linux:
-`curl -fsSL https://jont.tk/ventoytool > /tmp/ventoytool && bash /tmp/ventoytool`
+`curl -fsSL https://go.jontes.page/ventoy-tool/linux.sh > /tmp/ventoytool && bash /tmp/ventoytool`
